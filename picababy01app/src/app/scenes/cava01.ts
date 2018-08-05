18eccf67f5de6726ac12a6a36237f3121ad1465b
carrascoMDD <carrascomdd@gmail.com>
2018-08-05 18:50:57 +0200

    OK create cava01

--- picababy01app/src/app/scenes/cava01.ts
+++ picababy01app/src/app/scenes/cava01.ts
@@ -1,22 +1,17 @@
 import {
+    int, double,
+    Vector2, Vector3,
     Engine, Scene, FreeCamera, Light,
-    Vector3, HemisphericLight, MeshBuilder
+    MeshBuilder, VertexData, Mesh,
+    HemisphericLight,
+    ArcRotateCamera
 } from 'babylonjs';
 
-import Polygon = BABYLON.Polygon;
-import Mesh = BABYLON.Mesh;
-import double = BABYLON.double;
-import Vector2 = BABYLON.Vector2;
-import ArcRotateCamera = BABYLON.ArcRotateCamera;
 
 
-
-const SUBSTRACTFROM_X=713250;
-const SUBSTRACTFROM_Y=4361200;
-const FIXEDZ=0;
-
-
-const BORDERSTEPS_DEFAULT = 32;
+const BORDER_RADIUS = 1;
+const BORDER_STEPS_DEFAULT = 32;
+const SHEET_STEPS_DEFAULT  = 96;
 
 
 export class Cava01 {
@@ -25,7 +20,10 @@ export class Cava01 {
     private _scene: Scene;
     private _camera: FreeCamera;
     private _light: Light;
-    private _border: Mesh;
+    private _borderMesh: Mesh;
+    private _borderPoints: Vector3[];
+    private _sheetMesh: Mesh;
+    private _sheetVertexData: VertexData;
 
 
 
@@ -34,31 +32,6 @@ export class Cava01 {
         this._canvas = <HTMLCanvasElement>document.getElementById( canvasElement );
         this._engine = new Engine( this._canvas, true );
     }
-
-
-
-
-    createScene(): void {
-        // create a basic BJS Scene object
-        this._scene = new Scene( this._engine );
-
-
-        var camera = new ArcRotateCamera("cam", -Math.PI / 2, Math.PI / 2, 3, Vector3.Zero(), this._scene);
-
-        camera.wheelDeltaPercentage = 0.01;
-        camera.attachControl( this._canvas, true);
-
-        // create a basic light, aiming 0,1,0 - meaning, to the sky
-        this._light = new HemisphericLight( 'light1', new Vector3( 0, 1, 0 ), this._scene );
-
-        let someBorderPoints = this.cavaBorder( 1, 0.01, Math.PI);
-
-        this._border = MeshBuilder.CreateLines( "cavaBorder01", { points: someBorderPoints});
-
-
-    }
-
-
 
 
     animate(): void {
@@ -74,14 +47,52 @@ export class Cava01 {
     }
 
 
-    cavaBorder( theRadius: double, theMaxStepLen: double, theAngle: double): Vector3[] {
 
-        const someBorderPoints: Vector3[] = [];
+    createScene(): void {
+        // create a basic BJS Scene object
+        this._scene = new Scene( this._engine );
+
+        var camera = new ArcRotateCamera("cam", -Math.PI / 2, Math.PI / 2, 3, Vector3.Zero(), this._scene);
+        camera.wheelDeltaPercentage = 0.01;
+        camera.attachControl( this._canvas, true);
+
+        // create a basic light, aiming 0,1,0 - meaning, to the sky
+        this._light = new HemisphericLight( 'light1', new Vector3( 0, 1, 0 ), this._scene );
+
+        // Add and keep elements
+        this._borderMesh = this.cavaBorder( 1, 0.01);
+
+        this._sheetMesh = this.cavaSheet( 1, 5, 0.01);
+    }
+
+
+
+
+
+    cavaBorder( theRadius: double, theMaxStepLen: double): Mesh {
+        const someBorderPoints = this.cavaBorderPoints( theRadius, theMaxStepLen, Math.PI);
+        return MeshBuilder.CreateLines( "cavaBorder01", { points: someBorderPoints});
+    }
+
+
+    cavaBorderPoints( theRadius: double, theMaxStepLen: double, theAngle: double): Vector3[] {
+
+        if( !( this._borderPoints == null)) {
+            return this._borderPoints;
+        }
+
+        this._borderPoints = this.cavaBorderPoints_calc( theRadius, theMaxStepLen, theAngle);
+        return this._borderPoints;
+    }
+
+
+    cavaBorderPoints_calc( theRadius: double, theMaxStepLen: double, theAngle: double): Vector3[] {
+        const someBorderPoints: Vector3[] = [ ];
 
         const aCircleLen = 2 * Math.PI * theRadius;
         const anArcLen = aCircleLen * ( theAngle / ( 2 * Math.PI));
 
-        let aNumSteps = BORDERSTEPS_DEFAULT;
+        let aNumSteps = BORDER_STEPS_DEFAULT;
         const aTileLen = anArcLen / aNumSteps;
         if( aTileLen > theMaxStepLen) {
             aNumSteps = anArcLen / theMaxStepLen;
@@ -99,7 +110,7 @@ export class Cava01 {
             someBorderPoints.push( aBorderPoint);
         }
 
-        console.log( "someBorderPoints.length=" + someBorderPoints.length)
+        console.log( "someBorderPoints.length=" + someBorderPoints.length);
         return someBorderPoints;
     }
 
@@ -110,7 +121,124 @@ export class Cava01 {
         let anY = theRadius * Math.sin( theAngle);
         anX += theCenter.x;
         anY += theCenter.y;
+
         return new Vector3( anX, anY, 0);
     }
+
+
+
+
+
+
+    cavaSheet( theRadius: double, theLength: double, theMaxStepLen: double): Mesh {
+
+        const aVertexData = this.cavaSheetVertexData( theRadius, theLength, theMaxStepLen);
+        const aSheetMesh = new Mesh( "cavaSheet01", this._scene);
+        aVertexData.applyToMesh( aSheetMesh);
+
+        return aSheetMesh;
+    }
+
+
+
+
+    cavaSheetVertexData( theRadius: double, theLength: double, theMaxStepLen: double): VertexData {
+
+        if( !( this._sheetVertexData == null)) {
+            return this._sheetVertexData;
+        }
+
+        this._sheetVertexData = this.cavaSheetVertexData_calc( theRadius, theLength, theMaxStepLen);
+        return this._sheetVertexData;
+    }
+
+
+
+
+    cavaSheetVertexData_calc( theRadius: double, theLength: double, theMaxStepLen: double): VertexData {
+
+        let aNumStruts = SHEET_STEPS_DEFAULT;
+        const aStrutLen = theLength / aNumStruts;
+        if( aStrutLen > theMaxStepLen) {
+            aNumStruts = theLength / theMaxStepLen;
+            if( !( Math.floor( aNumStruts) == aNumStruts)) {
+                aNumStruts = Math.floor( aNumStruts) + 1;
+            }
+        }
+
+        const somePositions: double[] = [ ];
+        const someIndices:   int[]    = [ ];
+
+        const someBorderPoints = this.cavaBorderPoints( theRadius, 0.01, Math.PI);
+        const aNumBorderPoints = someBorderPoints.length;
+
+        // Add positions for the first strut from the border with Z = 0. Each triple of these is a point in the first strut of the sheet
+        for( let aBorderPointIdx=0; aBorderPointIdx < aNumBorderPoints; aBorderPointIdx++) {
+            const aBorderPoint = someBorderPoints[ aBorderPointIdx];
+            somePositions.push( aBorderPoint.x);
+            somePositions.push( aBorderPoint.y);
+            somePositions.push( 0);
+        }
+
+        // Add positions for all the struts but the first as positions from the border displaced in Z by a Strut len
+        for( let aStrutIdx=0; aStrutIdx < aNumStruts; aStrutIdx++) {
+
+            // Add positions from the border displaced in Z by a Strut lengh. Each triple of these is a point in a strut of the sheet
+            const aZ = aStrutIdx * theLength / aNumStruts;
+            for( let aBorderPointIdx=0; aBorderPointIdx < aNumBorderPoints; aBorderPointIdx++) {
+                const aBorderPoint = someBorderPoints[ aBorderPointIdx];
+                somePositions.push( aBorderPoint.x);
+                somePositions.push( aBorderPoint.y);
+                somePositions.push( aZ);
+            }
+        }
+
+        // Add indices for the facets between all struts, starting between the strut index 1 and the border (strut 0)
+        /* Two triangle facets, counter-clock-wise
+          m = n - 1
+          i = j - 1
+            SmVi-----------SmVj
+                | facet  /|
+                | 0     / |
+                |      /  |
+                |     /   |
+                |    /    |
+                |   /     |
+                |  /      |
+                | /     1 |
+                |/  facet |
+            SnVi-----------SnVj
+
+                facet 0:
+                    SmVi, SnVi, SmVj
+                facet 1:
+                    SmVj,SnVi, SnVj
+        */
+        for( let aStrutIdx=1; aStrutIdx < aNumStruts; aStrutIdx++) {
+
+            for( let aBorderPointIdx=1; aBorderPointIdx < aNumBorderPoints; aBorderPointIdx++) {
+
+                const anInd_SmVi = ( ( aStrutIdx - 1 ) * aNumBorderPoints) + ( aBorderPointIdx - 1);
+                const anInd_SmVj = ( ( aStrutIdx - 1 ) * aNumBorderPoints) + aBorderPointIdx;
+                const anInd_SnVi = ( aStrutIdx * aNumBorderPoints) + ( aBorderPointIdx - 1);
+                const anInd_SnVj = ( aStrutIdx * aNumBorderPoints) + aBorderPointIdx;
+
+                // Facet 0
+                someIndices.push( anInd_SmVi, anInd_SnVi, anInd_SmVj);
+                // Facet1
+                someIndices.push( anInd_SmVj, anInd_SnVi, anInd_SnVj);
+            }
+
+        }
+
+
+        const aVertexData = new VertexData();
+        aVertexData.positions = somePositions;
+        aVertexData.indices   = someIndices;
+
+        return aVertexData;
+    }
+
+
 
 }
